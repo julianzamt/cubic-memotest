922327b9ff5c893c16ef3eb3ac3d8ae9ab2ddf20
Julian Zamt <julizamt@gmail.com>
2021-03-27 13:13:39 -0300

    instructions controls

--- src/components/InstructionsModal.js
+++ src/components/InstructionsModal.js
@@ -6,6 +6,8 @@ import instructions_2 from "../img/instructions/instructions_2.png"
 import instructions_3 from "../img/instructions/instructions_3.png"
 import instructions_4 from "../img/instructions/instructions_4.png"
 import IconButton from '@material-ui/core/IconButton';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import CloseIcon from '@material-ui/icons/Close';
 import "./InstructionsModal.css"
 
@@ -18,7 +20,10 @@ function InstructionsModal(props) {
     };
 
     const handleChange = (event, value) => {
-        setPage(value);
+        const id = event.target.id
+        if (value) setPage(value);
+        else if (id === "next") setPage(2);
+        else setPage(1);
     };
     return (
         <Dialog onClose={handleClose} aria-labelledby="instructions" open={open}>
@@ -28,21 +33,27 @@ function InstructionsModal(props) {
             <div className="instructions__container">
                 {page === 1 ?
                     <div className="page__container">
-                        <img src={instructions_1} alt="instructions_1" className="instructionsImg" />
+                        <IconButton className="nextButton">
+                            <NavigateNextIcon id="next" onClick={handleChange} />
+                        </IconButton>
+                        <img src={instructions_1} alt="instructions_1" className="instructionsImg" onDragStart={(e) => { e.preventDefault() }} />
                         <div className="instructions__text">Drag the object</div>
-                        <img src={instructions_2} alt="instructions_2" className="instructionsImg" />
+                        <img src={instructions_2} alt="instructions_2" className="instructionsImg" onDragStart={(e) => { e.preventDefault() }} />
                         <div className="instructions__text">Tap or click cards to reveal images</div>
-                        <img src={instructions_3} alt="instructions_3" className="instructionsImg" />
+                        <img src={instructions_3} alt="instructions_3" className="instructionsImg" onDragStart={(e) => { e.preventDefault() }} />
                         <div className="instructions__text">Found matching pairs. <br></br>If so, images remain visible. If don´t, they hide again.</div>
                     </div>
                     :
                     <div className="page__container">
+                        <IconButton className="prevButton">
+                            <NavigateBeforeIcon id="previous" onClick={handleChange} />
+                        </IconButton>
                         <div style={{ textAlign: "center" }}>
                             <div className="timeTries">Time / Tries</div>
                             <div className="instructions__text">But stay focused, and be fast! You have limited resources.</div>
                         </div>
                         <div style={{ textAlign: "center" }}>
-                            <img src={instructions_4} alt="instructions_4" className="instructionsImg" />
+                            <img src={instructions_4} alt="instructions_4" className="instructionsImg" onDragStart={(e) => { e.preventDefault() }} />
                             <div className="instructions__text">Found all matches, and win.<br></br>Prepare yourself for the next level. <br></br><span style={{ fontSize: "smaller", color: "gray" }}>(Demo: 1 level only)</span></div>
                         </div>
                         <div style={{ textAlign: "center" }}>
